8ac9680213201e87ac92400cefc4baa572538664
iki <ik1ezz@gmail.com>
2012-11-07 14:40:56 +0600

    редактирование, перетаскивание задач

--- public/html-proto/js/app/app.task.js
+++ public/html-proto/js/app/app.task.js
@@ -2,14 +2,31 @@ $(function() {
     $('.task-edit-header').editable('http://www.example.com/save.php', {
         indicator : 'Saving...',
         tooltip   : 'Double click to edit...',
-        event     : 'dblclick'
+        event     : 'dblclick',
+        style     : "inherit"
     });
+    
     $('.task-edit-body').editable('http://www.example.com/save.php', {
         indicator : 'Saving...',
         tooltip   : 'Double click to edit...',
         event     : 'dblclick',
         submit    : 'Save',
         cancel    : 'Cancel',
-        type      : 'textarea'
+        type      : 'textarea',
+        style     : "inherit"
     });
+
+    $(".task-edit-performer").editable("save.php", {
+        indicator : 'Saving...',
+        tooltip   : 'Click to edit...',
+        data      : "{'deadkrolik':'deadkrolik','Pblcb':'Pblcb','raplos':'raplos','dilanmckey':'dilanmckey','iki':'iki'}",
+        type      : "select",
+        submit    : "OK",
+        style     : "inherit",
+        submitdata: function() {
+            return {id : 2};
+        }
+    }); 
+
+    $(".draggable").draggable({snap: ".colum-line_task", snapMode: "inner", snapTolerance: 50});
 });
