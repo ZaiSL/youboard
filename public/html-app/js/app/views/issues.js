7fab8612b9ffff6c42150eb176650eef274c4d03
iki <ik1ezz@gmail.com>
2012-11-22 10:38:10 +0600

    рендер фичи

--- public/html-app/js/app/views/issues.js
+++ public/html-app/js/app/views/issues.js
@@ -51,7 +51,7 @@ App.FeatureView = Backbone.View.extend({
 
 
     render : function (){
-
+	    this.$el.html(this.template.layout(this.model.toJSON()));
         return this;
     }
 
@@ -87,6 +87,11 @@ App.IssuesGroupView = Backbone.View.extend({
         if (this.model){
             //рендерим фичу
             // prependTo(this.$el);
+
+	        var featureView = new App.FeatureView({
+		        model : this.model
+	        });
+	        featureView.render().$el.prependTo(this.$el);
         }
 
 
